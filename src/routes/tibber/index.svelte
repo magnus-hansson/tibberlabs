4cf30bc9f0f390f2b2293d20fe202b3dabde6bcc
Magnus Hansson <hanssmag+git@gmail.com>
2022-08-11 18:12:25 +0200

    initial

---
+++ src/routes/tibber/index.svelte
@@ -0,0 +1,106 @@
+<script>
+	import { onMount } from 'svelte';
+	import item from './nodes.json';
+	import consumptiondata from './consumption.json';
+
+	import '@carbon/styles/css/styles.css';
+	import '@carbon/charts/styles.css';
+	import { BarChartSimple, HistogramChart } from '@carbon/charts-svelte';
+
+	let nodes = [];
+	let arrangedData = [];
+	let consumption = [{ from: '2022-08-11', cost: 55 }];
+	let kaj = [];
+	let test = [];
+	let query = `{
+		viewer {
+			homes {
+			consumption(resolution: HOURLY, last: 10) {
+				nodes {
+				from
+				to
+				cost
+				unitPrice
+				unitPriceVAT
+				consumption
+				consumptionUnit
+				}
+			}
+			}
+		}
+	}`;
+
+	onMount(async () => {
+		/* const res = await fetch('https://api.tibber.com/v1-beta/gql', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer K0GseNMSHm7EwtCVv29z0vawzqaEmdoT263DV2xIL3A'
+			},
+			body: JSON.stringify({ query })
+		});
+		let jsonres = await res.json();
+		console.log(jsonres.data.viewer.homes[0].consumption.nodes);
+		nodes = jsonres.data.viewer.homes[0].consumption.nodes;    */
+		//nodes = item.nodes;
+		/* arrangedData = nodes.map((x) => {
+			return { group: x.from, value: x.unitPrice };
+		}); */
+
+		kaj = await consumptiondata.consumption.nodes.map((x) => {
+			return {
+				group: x.from,
+				date: x.from,
+				value: x.unitPrice,
+				cost: x.cost,
+				consumption: x.consumption
+			};
+		});
+		console.log(kaj);
+		//console.log(arrangedData);
+		//console.log(nodes);
+	});
+</script>
+
+<svelte:head>
+	<title>Todos</title>
+	<meta name="description" content="A tibber app" />
+</svelte:head>
+
+<div class="todos">
+	<ul>
+		<!-- 	{#each arrangedData as d}
+			<li>{d.value}</li>
+		{/each} -->
+	</ul>
+
+	<BarChartSimple
+		data={kaj}
+		options={{
+			title: 'Tibber',
+			height: '600px',
+			axes: {
+				left: { mapsTo: 'cost' },
+				bottom: { mapsTo: 'date', scaleType: 'labels' }
+			}
+		}}
+	/>
+
+	<!-- <BarChartSimple
+		data={[
+			{ group: 'Qty', value: 65000 },
+			{ group: 'More', value: 29123 },
+			{ group: 'Sold', value: 35213 },
+			{ group: 'Restocking', value: 51213 },
+			{ group: 'Misc', value: 16932 }
+		]}
+		options={{
+			title: 'Simple bar (discrete)',
+			height: '400px',
+			axes: {
+				left: { mapsTo: 'value' },
+				bottom: { mapsTo: 'group', scaleType: 'labels' }
+			}
+		}}
+	/> -->
+</div>
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	ssr: {
+        noExternal: ['@carbon/charts', 'carbon-components'],
+    },
+};
+
+export default config;

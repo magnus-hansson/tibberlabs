7bf1b9e163e69c2680766ad336810154b8e4cbb0
Magnus Hansson <hanssmag+git@gmail.com>
2022-08-18 21:43:00 +0200

    current price

--- src/routes/tibber/index.svelte
+++ src/routes/tibber/index.svelte
@@ -1,15 +1,16 @@
 <script>
-	import { onMount } from 'svelte';
-	import item from './nodes.json';
-	import consumptiondata from './consumption.json';
+  import { onMount } from 'svelte'
+  import item from './nodes.json'
+  import consumptiondata from './consumption.json'
+  import current from './current.json'
 
-	import '@carbon/styles/css/styles.css';
-	import '@carbon/charts/styles.css';
-	import { BarChartSimple, HistogramChart } from '@carbon/charts-svelte';
+  import '@carbon/styles/css/styles.css'
+  import '@carbon/charts/styles.css'
+  import { BarChartSimple, LineChart } from '@carbon/charts-svelte'
 
-	let kaj = [];
-
-	let query = `{
+  let consumption = []
+  let currentPriceKrKW = 0
+  let query = `{
 		viewer {
 			homes {
 			consumption(resolution: HOURLY, last: 10) {
@@ -25,10 +26,16 @@
 			}
 			}
 		}
-	}`;
+	}`
 
-	onMount(async () => {
-		/* const res = await fetch('https://api.tibber.com/v1-beta/gql', {
+  const loadCurrent = async () => {
+    const currentPrice =
+      current.data.viewer.homes[0].currentSubscription.priceInfo.current.total
+    currentPriceKrKW = currentPrice
+  }
+
+  onMount(async () => {
+    /* const res = await fetch('https://api.tibber.com/v1-beta/gql', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -39,65 +46,50 @@
 		let jsonres = await res.json();
 		console.log(jsonres.data.viewer.homes[0].consumption.nodes);
 		nodes = jsonres.data.viewer.homes[0].consumption.nodes;    */
-		//nodes = item.nodes;
-		/* arrangedData = nodes.map((x) => {
+    //nodes = item.nodes;
+    /* arrangedData = nodes.map((x) => {
 			return { group: x.from, value: x.unitPrice };
 		}); */
 
-		kaj = await consumptiondata.consumption.nodes.map((x) => {
-			return {
-				group: 'Date',
-				date: x.from,
-				value: x.unitPrice,
-				cost: x.cost,
-				consumption: x.consumption
-			};
-		});
-		console.log(kaj);
-		//console.log(arrangedData);
-		//console.log(nodes);
-	});
+    consumption = await consumptiondata.consumption.nodes.map((x) => {
+      return {
+        group: 'Date',
+        date: x.from,
+        value: x.unitPrice,
+        cost: x.cost,
+        consumption: x.consumption
+      }
+    })
+
+    await loadCurrent()
+    //console.log(arrangedData);
+    //console.log(nodes);
+  })
 </script>
 
 <svelte:head>
-	<title>Todos</title>
-	<meta name="description" content="A tibber app" />
+  <title>Tibber!</title>
+  <meta name="description" content="A tibber app" />
 </svelte:head>
 
 <div class="todos">
-	<ul>
-		<!-- 	{#each arrangedData as d}
+  <ul>
+    <!-- 	{#each arrangedData as d}
 			<li>{d.value}</li>
 		{/each} -->
-	</ul>
+  </ul>
 
-	<BarChartSimple
-		data={kaj}
-		options={{
-			title: 'Tibber',
-			height: '600px',
-			axes: {
-				left: { mapsTo: 'cost' },
-				bottom: { mapsTo: 'date', scaleType: 'time' }
-			}
-		}}
-	/>
+  <div><h1>Just nu {currentPriceKrKW} kr/kwH</h1></div>
 
-	<!-- <BarChartSimple
-		data={[
-			{ group: 'Qty', value: 65000 },
-			{ group: 'More', value: 29123 },
-			{ group: 'Sold', value: 35213 },
-			{ group: 'Restocking', value: 51213 },
-			{ group: 'Misc', value: 16932 }
-		]}
-		options={{
-			title: 'Simple bar (discrete)',
-			height: '400px',
-			axes: {
-				left: { mapsTo: 'value' },
-				bottom: { mapsTo: 'group', scaleType: 'labels' }
-			}
-		}}
-	/> -->
+  <LineChart
+    data={consumption}
+    options={{
+      title: 'Kostnad per dag',
+      height: '400px',
+      axes: {
+        left: { mapsTo: 'cost' },
+        bottom: { mapsTo: 'date', scaleType: 'time' }
+      }
+    }}
+  />
 </div>
